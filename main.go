cd05a5b983a1bb8062919fd3705ced6dcda19510
Akshar Patel <AksharDPatel2004@gmail.com>
2025-12-01 23:54:10 -0500

    CH1, L8, A1

--- main.go
+++ main.go
@@ -8,6 +8,7 @@ import (
 func main() {
 	mux := http.NewServeMux()
 	mux.Handle("/", http.FileServer(http.Dir(".")))
+	mux.Handle("/assets/", http.FileServer(http.Dir(".")))
 	serverStruct := http.Server{
 		Handler: mux,
 		Addr:    ":8080",
